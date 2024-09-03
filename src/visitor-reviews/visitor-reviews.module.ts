bfd0281a6947c034ceb40e1e790e25e0785a113a
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-09-03 14:00:55 +0100

    module visitor-reviews

---
+++ src/visitor-reviews/visitor-reviews.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { VisitorReviewsService } from './visitor-reviews.service';
+import { VisitorReviewsController } from './visitor-reviews.controller';
+import { DatabaseModule } from 'src/database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [VisitorReviewsController],
+  providers: [VisitorReviewsService],
+  exports: [VisitorReviewsService],
+})
+export class VisitorReviewsModule { }
---
+++ src/visitor-reviews/visitor-reviews.service.ts
@@ -0,0 +1,67 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateVisitorReviewDto } from './dto/create-visitor-review.dto';
+import { DataSource } from 'typeorm';
+import { VisitorReview } from './entities/visitor-review.entity';
+
+@Injectable()
+export class VisitorReviewsService {
+  constructor(
+    @Inject("DATA_SOURCE")
+    private dataSource: DataSource,
+  ) { }
+
+  async create(createVisitorReviewDto: CreateVisitorReviewDto) {
+    createVisitorReviewDto.isVisible = false;
+    return await this.dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(VisitorReview)
+      .values(createVisitorReviewDto)
+      .execute();
+  }
+
+  async findAll(): Promise<VisitorReview[]> {
+    return await this.dataSource
+      .getRepository(VisitorReview)
+      .createQueryBuilder('visitorReview')
+      .getMany();
+  }
+
+  async findAcceptedReviews(): Promise<VisitorReview[]> {
+    return await this.dataSource
+      .getRepository(VisitorReview)
+      .createQueryBuilder('visitorReview')
+      .where("visitorReview.isVisible = :isVisible", { isVisible: true })
+      .getMany();
+  }
+
+  async findOne(id: number): Promise<VisitorReview> {
+    return await this.dataSource
+      .getRepository(VisitorReview)
+      .createQueryBuilder('visitorReview')
+      .where("visitorReview.id = :id", { id: id })
+      .getOne();
+  }
+
+  async acceptReview(id: number): Promise<void> {
+    await this.dataSource
+      .createQueryBuilder()
+      .update(VisitorReview)
+      .set({
+        isVisible: true
+      })
+      .where("id = :id", { id: id })
+      .execute();
+  }
+
+  async rejectReview(id: number): Promise<void> {
+    await this.dataSource
+      .createQueryBuilder()
+      .update(VisitorReview)
+      .set({
+        isVisible: false
+      })
+      .where("id = :id", { id: id })
+      .execute();
+  }
+}
