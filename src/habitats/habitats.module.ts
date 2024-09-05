8bc7bf43c0f9913e0024f6eb7cdcc55cc75985fa
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-09-05 17:47:04 +0100

    Ajustement du module habitats (inclure les images)

--- src/habitats/habitats.module.ts
+++ src/habitats/habitats.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { HabitatsService } from './habitats.service';
 import { HabitatsController } from './habitats.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { FilesService } from 'src/files/files.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [HabitatsController],
-  providers: [HabitatsService],
+  providers: [HabitatsService, FilesService],
   exports: [HabitatsService],
 })
 export class HabitatsModule { }
