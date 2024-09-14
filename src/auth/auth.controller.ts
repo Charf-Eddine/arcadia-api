4d53ebb6f41e878439596e8fd766a2509348e767
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-09-14 16:25:59 +0200

    implémentation du passport jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Request, Post, UseGuards, HttpCode } from '@nestjs/common';
+
+import { Controller, Request, Post, UseGuards, HttpCode, Get, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiInternalServerErrorResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LoginDto } from './dto/login.dto';
 
 @ApiTags("Auth")
 @Controller("auth")
-@Controller()
 export class AuthController {
-
+    constructor(private authService: AuthService) {}
+    
     @ApiOkResponse({ description: "Login successfully." })
     @ApiBadRequestResponse({ description: "Params are wrong." })
     @ApiInternalServerErrorResponse({ description: "Internal server error." })
@@ -14,7 +18,14 @@ export class AuthController {
     @UseGuards(AuthGuard('local'))
     @Post('login')
     @HttpCode(200)
-    async login(@Request() req) {
-    return req.user;
+    async login(@Body() login: LoginDto, @Request() req) {
+        return this.authService.login(req.user);
+    }
+
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
     }
 }
