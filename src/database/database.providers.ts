c6f53cf3e0f6cfae00f10a5ce6f630e8cca650b3
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-11-14 13:38:01 +0100

    lecture des paramètres de connexion à partir des variables d'environnment

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,23 +1,25 @@
+import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
-    {
-        provide: 'DATA_SOURCE',
-        useFactory: async () => {
-            const dataSource = new DataSource({
-                type: 'mysql',
-                host: 'localhost',
-                port: 3306,
-                username: 'root',
-                password: '',
-                database: 'arcadia',
-                entities: [
-                    __dirname + '/../**/*.entity{.ts,.js}',
-                ],
-                synchronize: false,
-            });
+  {
+    provide: 'DATA_SOURCE',
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: "mysql",
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
+        entities: [
+          __dirname + "/../**/*.entity{.ts,.js}",
+        ],
+        synchronize: false,
+      });
 
-            return dataSource.initialize();
-        },
+      return dataSource.initialize();
     },
+  },
 ];
